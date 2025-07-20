70c27f8a09051fc9c1c079597d9e5c33f2f4e4f7
AbbuGunasekhar <155729999+AbbuGunasekhar@users.noreply.github.com>
2025-07-20 20:48:46 +0530

    Add files via upload

---
+++ DAM_PROJECT/LOGIN/LOGIN.js
@@ -0,0 +1,29 @@
+// var body=document.querySelector(".body");
+// var body2=document.querySelector(".body2");
+
+// var bgimage=document.querySelector(".bgimage");
+// var homesignin=document.getElementById("home-signin");
+// var homesignup=document.getElementById("home-signup");
+
+// function signin()
+// {
+//     body.style.display="flex";
+   
+
+//     body2.style.display="none";
+//     bgimage.style.display="none";
+//     // homesignin.style.display="none";
+//     //  homesignup.style.display="none";
+// }
+
+
+// function signup()
+// {
+   
+//     body2.style.display="flex";
+    
+
+//     body.style.display="none";
+//     // homesignin.style.display="none";
+//     //  homesignup.style.display="none";
+// }
